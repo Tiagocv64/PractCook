47ddbcf7b2aea4152c009c2041c4b090bd1882b8
Tiagocv64 <tcverdade@hotmail.com>
2017-08-18 01:05:51 +0100

    Added Login page

--- app/src/main/java/com/example/tcver/practcook/LoginActivity.java
+++ app/src/main/java/com/example/tcver/practcook/LoginActivity.java
@@ -1,13 +1,88 @@
 package com.example.tcver.practcook;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 321;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+            Intent intentMain = new Intent(this, MainActivity.class);
+            startActivity(intentMain);
+            finish();
+        } else {
+            // not signed in
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(
+                                    Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
+                            .setLogo(R.drawable.pract_cook_image)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    //Result of Login's Activity
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == ResultCodes.OK) {
+                Intent intentMain = new Intent(this, MainActivity.class);
+                startActivity(intentMain);
+                finish();
+                return;
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    showSnackbar(R.string.sign_in_cancelled);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackbar(R.string.unknown_error);
+                    return;
+                }
+            }
+
+            showSnackbar(R.string.unknown_sign_in_response);
+        }
+    }
+
+    protected void showSnackbar(int messageSnackbar){
+        Snackbar snackbar = Snackbar
+                .make(findViewById(R.id.layout_login_activity) , getResources().getString(messageSnackbar), Snackbar.LENGTH_SHORT);
+
+        snackbar.show();
     }
 }
