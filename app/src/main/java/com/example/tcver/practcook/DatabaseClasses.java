89f6919cbfeef8dff67d5d200ad0859a1b0dafdb
Rodrigo Rosmaninho <quico.rosmaninho@gmail.com>
2017-09-12 04:16:22 +0100

    Initiated work on showing a list of friends and adding them

--- app/src/main/java/com/example/tcver/practcook/DatabaseClasses.java
+++ app/src/main/java/com/example/tcver/practcook/DatabaseClasses.java
@@ -3,64 +3,64 @@ package com.example.tcver.practcook;
 public class DatabaseClasses {
 
     // Isto é um pouco estranho mas tem que ser assim
-// VER: https://firebase.google.com/docs/database/android/read-and-write
+    // VER: https://firebase.google.com/docs/database/android/read-and-write
 
     static class User {
-        private String Bio;
-        private String Email;
-        private String Foto;
-        private Object Friends;
-        private String ID;
-        private int Level;
-        private String Name;
-        private Object Receitas;
+        private String bio;
+        private String email;
+        private String foto;
+        private Object friends;
+        private String id;
+        private int level;
+        private String name;
+        private Object receitas;
 
         public User() {
         }
 
         // Isto é assim simplesmente porque tem que ser. Copia
-        public User(String Bio, String Email, String Foto, Object Friends, String ID, int Level, String Name, Object Receitas) {
-            this.Bio = Bio;
-            this.Email = Email;
-            this.Foto = Foto;
-            this.Friends = Friends;
-            this.ID = ID;
-            this.Level = Level;
-            this.Name = Name;
-            this.Receitas = Receitas;
+        public User(String bio, String email, String foto, Object friends, String id, int level, String name, Object receitas) {
+            this.bio = bio;
+            this.email = email;
+            this.foto = foto;
+            this.friends = friends;
+            this.id = id;
+            this.level = level;
+            this.name = name;
+            this.receitas = receitas;
 
         }
 
         public String getBio(){
-            return Bio;
+            return bio;
         }
 
         public String getEmail() {
-            return Email;
+            return email;
         }
 
         public String getFoto() {
-            return Foto;
+            return foto;
         }
 
         public Object getFriends() {
-            return Friends;
+            return friends;
         }
 
         public String getID() {
-            return ID;
+            return id;
         }
 
         public int getLevel() {
-            return Level;
+            return level;
         }
 
         public String getName() {
-            return Name;
+            return name;
         }
 
         public Object getReceitas() {
-            return Receitas;
+            return receitas;
         }
     }
 }
--- app/src/main/java/com/example/tcver/practcook/FriendsFragment.java
+++ app/src/main/java/com/example/tcver/practcook/FriendsFragment.java
@@ -1,12 +1,28 @@
 package com.example.tcver.practcook;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.design.widget.FloatingActionButton;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class FriendsFragment extends Fragment {
 
@@ -17,14 +33,87 @@ public class FriendsFragment extends Fragment {
         ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Friends");
 
         View rootView = getActivity().getWindow().getDecorView().findViewById(android.R.id.content);
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
 
         // Selecionar o item correto na barra da esquerda
         NavigationView nav_view = (NavigationView) rootView.findViewById(R.id.nav_view);
         nav_view.getMenu().getItem(1).setChecked(true);
 
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.add_friends_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final MaterialDialog dialog = new MaterialDialog.Builder(view.getContext())
+                        .title("Search For Friends")
+                        .customView(R.layout.dialog_add_friends, true)
+                        .positiveText("Done")
+                        .build();
+
+                final EditText edit_txt = (EditText) dialog.getCustomView().findViewById(R.id.friends_search_bar);
+
+                edit_txt.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+                    @Override
+                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                        if (actionId == EditorInfo.IME_ACTION_DONE) {
+                            // CODE HERE
+                            String queryText = edit_txt.getText().toString();
+                            getList(queryText, dialog.getCustomView());
+                            return true;
+                        }
+                        return false;
+                    }
+                });
+
+                dialog.show();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        return view;
     }
 
+    public void getList(String queryText, View dialog) {
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
+        Query query = databaseReference.orderByChild("name")
+                .startAt(queryText)
+                .endAt(queryText + "\uf8ff");
+
+        RecyclerView people = (RecyclerView) dialog.findViewById(R.id.search_results);
+        people.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        FirebaseRecyclerAdapter mAdapter = new FirebaseRecyclerAdapter<DatabaseClasses.User, UserHolder>(DatabaseClasses.User.class, R.layout.populate_people, UserHolder.class, query) {
+            @Override
+            public void populateViewHolder(UserHolder holder, DatabaseClasses.User user, int position) {
+                holder.setName(user.getName(), user.getID());
+                holder.setBio(user.getBio());
+
+                // TODO: FOTO!!!
+                // holder.setFoto(user.getFoto());
+
+            }
+        };
+
+        people.setAdapter(mAdapter);
+    }
+
+    public static class UserHolder extends RecyclerView.ViewHolder {
+        private final TextView mNameField;
+        private final TextView mBioField;
+
+        public UserHolder(View itemView) {
+            super(itemView);
+            mNameField = (TextView) itemView.findViewById(R.id.personName);
+            mBioField = (TextView) itemView.findViewById(R.id.personBio);
+        }
+
+        public void setName(String name, String uID) {
+            mNameField.setText(name);
+        }
+
+        public void setBio(String message) {
+            mBioField.setText(message);
+        }
+    }
 
 }
