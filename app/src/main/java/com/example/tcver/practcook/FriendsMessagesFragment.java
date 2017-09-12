534b775e75f69cd2015b7d8d3b2cfa073e9b9306
Tiagocv64 <tcverdade@hotmail.com>
2017-09-12 03:48:15 +0100

    Added tabs and fragments in messages fragment

---
+++ app/src/main/java/com/example/tcver/practcook/FriendsMessagesFragment.java
@@ -0,0 +1,22 @@
+package com.example.tcver.practcook;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class FriendsMessagesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friends_messages, container, false);
+    }
+
+}
--- app/src/main/java/com/example/tcver/practcook/MessagesFragment.java
+++ app/src/main/java/com/example/tcver/practcook/MessagesFragment.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +13,9 @@ import android.view.ViewGroup;
 
 import com.roughike.bottombar.BottomBar;
 
-public class MessagesFragment extends Fragment {
+public class MessagesFragment extends Fragment{
+        private FragmentTabHost mTabHost;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,7 +37,28 @@ public class MessagesFragment extends Fragment {
             nav_view.getMenu().getItem(i).setChecked(false);
         }
 
+        View view = (View) inflater.inflate(R.layout.fragment_messages, container, false);
+
+        mTabHost = (FragmentTabHost) view.findViewById(android.R.id.tabhost);
+        mTabHost.setup(getActivity(), getChildFragmentManager(), android.R.id.tabcontent);
+
+        mTabHost.addTab(
+                mTabHost.newTabSpec("FriendsTab").setIndicator("Friends", null),
+                FriendsMessagesFragment.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("MessagesTab").setIndicator("Messages", null),
+                MessagesMessagesFragment.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("GroupsTab").setIndicator("Groups", null),
+                GroupsMessagesFragment.class, null);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messages, container, false);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mTabHost = null;
     }
 }
